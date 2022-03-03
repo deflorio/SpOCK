a3bf2bad6e6d28fbd861d11c734b20ddc30b24f0
Sergio De Florio <sdefly@yahoo.com>
2022-03-03 10:23:14 +0100

    Added SGP4 lib.

---
+++ extlib/SGP4/make.mk
@@ -0,0 +1,8 @@
+SELF_DIR = $(dir $(lastword $(MAKEFILE_LIST)))
+C_SRC += $(wildcard $(SELF_DIR)/*.c)
+CPP_SRC_SGP4 += $(wildcard $(SELF_DIR)/*.cpp)
+H_SRC += $(wildcard $(SELF_DIR)/*.h)
+INCLUDE_PATH += $ -I"$(SELF_DIR)"
+LIBRARY_PATH += #-L"$(SELF_DIR)"
+#include makefile fragments in subdirectories if they exist
+-include $(SELF_DIR)/*/make.mk
